20e225c59f3791f17a66298c618bb4583c862d27
9keyyyy <9keyyyy@gmail.com>
2023-04-18 01:13:55 +0900

    build: 프로젝트 세팅

---
+++ src/main/java/com/sejongmate/SejongmateApplication.java
@@ -0,0 +1,13 @@
+package com.sejongmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SejongmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejongmate/SejongmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejongmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
