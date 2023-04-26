6dc0eb01ac5ad50f3a8422abe0b7663a30544d62
9keyyyy <9keyyyy@gmail.com>
2023-04-26 19:51:45 +0900

    feat: 로그인 구현

--- src/main/java/com/sejongmate/user/presentation/UserController.java
+++ src/main/java/com/sejongmate/user/presentation/UserController.java
@@ -6,6 +6,8 @@ import com.sejongmate.common.BaseResponseStatus;
 import com.sejongmate.user.application.UserService;
 import com.sejongmate.user.presentation.dto.UserCreateReqDto;
 import com.sejongmate.user.presentation.dto.UserCreateResDto;
+import com.sejongmate.user.presentation.dto.UserLoginReqDto;
+import com.sejongmate.user.presentation.dto.UserLoginResDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -38,4 +40,21 @@ public class UserController {
             return new BaseResponse<>(e.getStatus());
         }
     }
+
+    @PostMapping("/login")
+    public BaseResponse<UserLoginResDto> login(@Valid @RequestBody UserLoginReqDto userLoginReqDto, BindingResult br) throws BaseException {
+
+        if (br.hasErrors()){
+            String errorName = br.getAllErrors().get(0).getDefaultMessage();
+            log.error(errorName);
+            return new BaseResponse<>(errorName);
+        }
+
+        try {
+            return new BaseResponse<>(userService.login(userLoginReqDto));
+        } catch(BaseException e){
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
 }
