a74e97d95993b30e3c4d902dbde4af45de4bac76
9keyyyy <9keyyyy@gmail.com>
2023-05-04 01:55:41 +0900

    feat: jwt config 설정

---
+++ src/main/java/com/sejongmate/config/security/JwtFilter.java
@@ -0,0 +1,54 @@
+package com.sejongmate.config.security;
+
+import com.sejongmate.authentication.domain.JwtTokenProvider;
+import com.sejongmate.common.BaseException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.sejongmate.common.BaseResponseStatus.INVALID_JWT;
+import static com.sejongmate.common.BaseResponseStatus.REDIS_ERROR;
+
+/**
+ * 헤더(Authorization)에 있는 토큰을 꺼내 이상이 없는 경우 SecurityContext에 저장
+ * Request 이전에 작동
+ */
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        try {
+            if (token != null && jwtTokenProvider.validateAccessToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth); // 정상 토큰이면 SecurityContext에 저장
+            }
+        } catch (RedisConnectionFailureException e) {
+            SecurityContextHolder.clearContext();
+            throw new BaseException(REDIS_ERROR);
+        } catch (Exception e) {
+            throw new BaseException(INVALID_JWT);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
