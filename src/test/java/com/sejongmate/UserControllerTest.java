f91b1c5bd1be400ee0385bfdd00b20d4b0e9302a
9keyyyy <9keyyyy@gmail.com>
2023-05-08 23:53:08 +0900

    test: api 테스트 코드 추가

---
+++ src/test/java/com/sejongmate/UserControllerTest.java
@@ -0,0 +1,19 @@
+package com.sejongmate;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    
+}
