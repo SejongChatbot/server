8c692037544a069979292573110ad51f179be016
9keyyyy <9keyyyy@gmail.com>
2023-05-19 16:36:40 +0900

    test: 채팅 API 테스트 코드 추가

--- src/main/java/com/sejongmate/chat/presentation/dto/ChatRoomReqDto.java
+++ src/main/java/com/sejongmate/chat/presentation/dto/ChatRoomReqDto.java
@@ -4,6 +4,7 @@ import com.sejongmate.chat.domain.ChatParticipant;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,4 +17,10 @@ public class ChatRoomReqDto {
     @NotNull(message = "채팅방 이름을 입력하세요.")
     private String name;
     private List<String> participantNum;
+
+    @Builder
+    public ChatRoomReqDto(String name, List<String> participantNum) {
+        this.name = name;
+        this.participantNum = participantNum;
+    }
 }
